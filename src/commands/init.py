4f0373200337451b72bd62b69f42b27105a63b78
MarinCervinschi <marincervinschi3@gmail.com>
2025-09-10 11:45:44 +0200

    feat(command): init

--- src/commands/init.py
+++ src/commands/init.py
@@ -1,3 +1,13 @@
-def cmd_init(args):
-    # TODO: implementazione
-    pass
+from src.core.repository import repo_create
+from argparse import Namespace
+
+
+def cmd_init(args: Namespace) -> None:
+    """
+    Handles the 'init' command for Vestigium CLI.
+
+    Initializes a new, empty repository at the specified path.
+    Args:
+        args (Namespace): Parsed command-line arguments. Must contain 'path'.
+    """
+    repo_create(args.path)
