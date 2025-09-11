e7cc1b90445016adc1bf66d282e768cc79529f6d
MarinCervinschi <marincervinschi3@gmail.com>
2025-09-11 14:39:23 +0200

    feat(command): hash object

--- src/commands/hash_object.py
+++ src/commands/hash_object.py
@@ -1,3 +1,26 @@
-def cmd_hash_object(args):
-    # TODO: implementazione
-    pass
+from argparse import Namespace
+
+from src.core.objects import object_hash
+from src.core.repository import repo_find
+
+
+def cmd_hash_object(args: Namespace) -> None:
+    """Hash an object and optionally write it to the repository.
+
+    Args:
+        args (Namespace): Command-line arguments containing:
+            - path (str): Path to the file to hash.
+            - type (str): Type of the object (e.g., 'blob', 'tree').
+            - write (bool): Whether to write the object to the repository.
+
+    Returns:
+        None. Prints the SHA-1 hash of the object.
+    """
+    if args.write:
+        repo = repo_find()
+    else:
+        repo = None
+
+    with open(args.path, "rb") as fd:
+        sha = object_hash(fd, args.type.encode(), repo)
+        print(sha)
