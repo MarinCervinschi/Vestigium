350c113c8a95f89135f753972cb43c8148601cbf
MarinCervinschi <marincervinschi3@gmail.com>
2025-09-11 09:49:14 +0200

    feat(objects): object_read

--- src/core/objects.py
+++ src/core/objects.py
@@ -1,8 +1,11 @@
 import zlib
 import hashlib
+import os
 from dataclasses import dataclass
 from abc import ABC, abstractmethod
 from src.core.repository import VesRepository
+from src.core.repository import repo_file
+from typing import Optional
 
 
 @dataclass
@@ -38,3 +41,76 @@ class VesObject(ABC):
 
     def init(self) -> None:
         pass
+
+
+def object_read(repo: VesRepository, sha: str) -> Optional[VesObject]:
+    path = repo_file(repo, "objects", sha[:2], sha[2:])
+
+    if path == None or not os.path.isfile(path):
+        return None
+
+    with open(path, "rb") as f:
+        raw = zlib.decompress(f.read())
+
+        object_type = raw.find(b" ")
+        fmt = raw[:object_type]
+
+        object_size = raw.find(b"\x00", object_type)
+        size = int(raw[object_type:object_size].decode("ascii"))
+        if size != len(raw) - object_size - 1:
+            raise Exception(f"Malformed object {sha}: bad length")
+
+        match fmt:
+            case b"commit":
+                c = GitCommit
+            case b"tree":
+                c = GitTree
+            case b"tag":
+                c = GitTag
+            case b"blob":
+                c = GitBlob
+            case _:
+                raise Exception(f"Unknown type {fmt.decode("ascii")} for object {sha}")
+
+        return c(raw[object_size + 1 :])
+
+
+class GitCommit(VesObject):
+
+    def serialize(self, repo: VesRepository) -> bytes:
+        # TODO: Implement commit serialization
+        return b""
+
+    def deserialize(self, data: bytes) -> None:
+        # TODO: Implement commit deserialization
+        return None
+
+
+class GitTree(VesObject):
+    def serialize(self, repo: VesRepository) -> bytes:
+        # TODO: Implement tree serialization
+        return b""
+
+    def deserialize(self, data: bytes) -> None:
+        # TODO: Implement tree deserialization
+        return None
+
+
+class GitTag(VesObject):
+    def serialize(self, repo: VesRepository) -> bytes:
+        # TODO: Implement tag serialization
+        return b""
+
+    def deserialize(self, data: bytes) -> None:
+        # TODO: Implement tag deserialization
+        return None
+
+
+class GitBlob(VesObject):
+    def serialize(self, repo: VesRepository) -> bytes:
+        # TODO: Implement blob serialization
+        return b""
+
+    def deserialize(self, data: bytes) -> None:
+        # TODO: Implement blob deserialization
+        return None
